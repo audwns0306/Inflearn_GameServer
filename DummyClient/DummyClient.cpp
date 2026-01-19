558d398a010e7b1aeef3e4c5a8cd0e7ecd60d09e
audwns0306 <cpyem0@naver.com>
2026-01-19 20:38:23 +0900

    38~40, 43
    소켓 프로그래밍, 소켓 옵션

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,7 +1,120 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <ws2tcpip.h>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+#pragma comment(lib, "ws2_32.lib")
+
+//	38~40. 소켓 프로그래밍
+
+//int main()
+//{
+//	//	윈속 초기화 (ws2_32 라이브러리 초기화)
+//	//	관련 정보가 wsaData에 채워짐
+//	WSAData wsaData;
+//	if(::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		return 0;
+//
+//	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+//	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+//	// protocol : 0
+//	// return : descriptor
+//
+//	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+//	if (clientSocket == INVALID_SOCKET)
+//	{
+//		int32 errCode = ::WSAGetLastError();
+//		std::cout << "Socket ErrorCode : " << errCode << std::endl;
+//
+//		return 0;
+//	}
+//
+//	SOCKADDR_IN serverAddr;	//	IPv4
+//	::memset(&serverAddr, 0, sizeof(serverAddr));
+//	serverAddr.sin_family = AF_INET;
+//	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
+//	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+//	serverAddr.sin_port = ::htons(7777);
+//
+//	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+//	{
+//		int32 errCode = ::WSAGetLastError();
+//		std::cout << "Connect ErrorCode : " << errCode << std::endl;
+//		return 0;
+//	}
+//
+//	// ---------------------------------------
+//	// 연결 성공! 이제부터 데이터 송수신 가능!
+//	// ---------------------------------------
+//	
+//	std::cout << "Connected To Server!" << std::endl;
+//
+//	while (true)
+//	{
+//
+//		char sendBuffer[100] = "Hello World!";
+//		int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+//
+//		if (resultCode == INVALID_SOCKET)
+//		{
+//			int32 errCode = ::WSAGetLastError();
+//			std::cout << "Send ErrorCode : " << errCode << std::endl;
+//			return 0;
+//		}
+//
+//		std::cout << "Send Data! Len = " << sizeof(sendBuffer) << std::endl;
+//
+//		char recvBuffer[1000];
+//
+//		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+//		if (recvLen <= 0)
+//		{
+//			int32 errCode = ::WSAGetLastError();
+//			std::cout << "Recv ErrorCode : " << errCode << std::endl;
+//			return 0;
+//		}
+//
+//		std::cout << "Recv Data! Data = " << recvBuffer << std::endl;
+//		std::cout << "Recv Data! Len = " << recvLen << std::endl;
+//
+//		std::this_thread::sleep_for(1s);
+//	}
+//
+//	// 소켓 리소스 반환
+//	::closesocket(clientSocket);
+//	
+//	// 윈속 종료
+//	::WSACleanup();
+//}
+
+//	43. 소켓 옵션
+
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	std::cout << cause << " ErrorCode : " << errCode << std::endl;
+}
 
 int main()
 {
-	HelloWorld();
+	WSAData wsaData;
+	if(::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		HandleError("Socket");
+		return 0;
+	}
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+	
+	// 윈속 종료
+	::WSACleanup();
 }
--- Inflearn_GameServer/Inflearn_GameServer.cpp
+++ Inflearn_GameServer/Inflearn_GameServer.cpp
@@ -841,7 +841,6 @@
 //	24. ThreadManager
 #include "ThreadManager.h"
 
-
 //CoreGlobal Core;
 //
 //using namespace std::chrono_literals;
@@ -944,11 +943,201 @@
 
 //	29. Smart Pointer
 
+//int main()
+//{
+//	
+//	//	우리가 만든 SharedPtr방식의 한계
+//	// 
+//	//	1) 이미 만들어진 클래스 대상으로 사용 불가
+//	//	2) 순환 (Cycle) 문제
+//}
+
+//	30.	Allocator
+
+//int main()
+//{
+//
+//}
+
+//	38~40. 소켓 프로그래밍
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+//int main()
+//{
+//	WSAData wsaData;
+//	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		return 0;
+//
+//	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+//	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+//	// protocol : 0
+//	// return : descriptor
+//
+//	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+//	if (listenSocket == INVALID_SOCKET)
+//	{
+//		int32 errCode = ::WSAGetLastError();
+//		std::cout << "Socket ErrorCode : " << errCode << std::endl;
+//		return 0;
+//	}
+//
+//	SOCKADDR_IN serverAddr;
+//	::memset(&serverAddr, 0, sizeof(serverAddr));
+//	serverAddr.sin_family = AF_INET;
+//	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+//	serverAddr.sin_port = ::htons(7777);
+//
+//	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+//	{
+//		int32 errCode = ::WSAGetLastError();
+//		std::cout << "Bind ErrorCode : " << errCode << std::endl;
+//		return 0;
+//	}
+//
+//	if(::listen(listenSocket, 10))
+//	{
+//		int32 errCode = ::WSAGetLastError();
+//		std::cout << "Listen ErrorCode : " << errCode << std::endl;
+//		return 0;
+//	}
+//
+//	// --------------------------
+//	
+//	while (true)
+//	{
+//		SOCKADDR_IN clientAddr;
+//		::memset(&clientAddr, 0, sizeof(clientAddr));
+//		int32 addrLen = sizeof(clientAddr);
+//
+//		SOCKET clientSocket = ::accept(listenSocket,(SOCKADDR*)&clientAddr, &addrLen);
+//		if (clientSocket == INVALID_SOCKET)
+//		{
+//			int32 errCode = ::WSAGetLastError();
+//			std::cout << "Accept ErrorCode : " << errCode << std::endl;
+//			return 0;
+//		}
+//
+//		char ipAddr[16];
+//		::inet_ntop(AF_INET, &clientAddr, ipAddr, sizeof(ipAddr));
+//
+//		std::cout << "Client Connected! IP = " << ipAddr << std::endl;
+//		
+//		
+//
+//		while (true)
+//		{
+//			char recvBuffer[1000];
+//
+//			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+//			if (recvLen <= 0)
+//			{
+//				int32 errCode = ::WSAGetLastError();
+//				std::cout << "Recv ErrorCode : " << errCode << std::endl;
+//				return 0;
+//			}
+//
+//			std::cout << "Recv Data! Data = " << recvBuffer << std::endl;
+//			std::cout << "Recv Data! Len = " << recvLen << std::endl;
+//
+//			int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
+//
+//			if (resultCode == INVALID_SOCKET)
+//			{
+//				int32 errCode = ::WSAGetLastError();
+//				std::cout << "Send ErrorCode : " << errCode << std::endl;
+//				return 0;
+//			}
+//
+//		}
+//	}
+//
+//	// --------------------------
+//	::WSACleanup();
+//}
+
+//	43.	소켓 옵션
+
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	std::cout << cause << " ErrorCode : " << errCode << std::endl;
+}
+
 int main()
 {
-	
-	//	우리가 만든 SharedPtr방식의 한계
-	// 
-	//	1) 이미 만들어진 클래스 대상으로 사용 불가
-	//	2) 순환 (Cycle) 문제
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET serverSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocket == INVALID_SOCKET)
+	{
+		HandleError("Socket");
+		return 0;
+	}
+
+
+	// 옵션을 해석하고 처리할 주체?
+	// 소켓 코드 -> SOL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP 프로토콜 -> IPPROTO_TCP
+
+	// SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부 (TCP Only)
+	// 상대방이 소리소문없이 연결 끊는다면?
+	// 주기적으로 TCP 프로토콜 연결 상태 확인 -> 끊어진 연결 감지
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+
+	// SO_LINGER = 지연하다
+	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
+	// onoff = 0이면 closesocket()이 바로 리턴, 아니면 linger초만큼 대기 (default 0)
+	// linger = 대기 시간
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+
+	// Half-Close
+	// SD_SEND : send 막는다
+	// SD_RECEIVE : recv 막는다
+	// SD_BOTH : 둘다 막는다
+	//::shutdown(serverSocket, SD_SEND);
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	// SO_RCVBUF = 수신 버퍼 크기
+	int32 sendBufferSize;
+	int32 optionLen = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	std::cout << "송신 버퍼 크기 : " << sendBufferSize << std::endl;
+
+	int32 recvBufferSize;
+	optionLen = sizeof(recvBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufferSize, &optionLen);
+	std::cout << "수신 버퍼 크기 : " << recvBufferSize << std::endl;
+
+	// SO_REUSEADDR
+	// IP주소 및 port 재사용
+	{
+		bool enable = true;
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
+	}
+
+	// IPPROTO_TCP
+	// TCP_NODELAY = Nagle 네이글 알고리즘 작동 여부
+	// 데이터가 충분히 크면 보내고, 그렇지 않으면 데이터가 충분히 쌓일때까지 대기
+	// 장점 : 작은 패킷이 불필요하게 많이 생성되는 일을 방지
+	// 단점 : 반응 시간 손해
+	// 일반적으로 NODELAY를 키면서 Nagle을 끈다
+	{
+		bool enable = true;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
+	}
+
+	::closesocket(serverSocket);
+
+	::WSACleanup();
 }
